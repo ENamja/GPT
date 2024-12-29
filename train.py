4ee973493ae45bf25e896d2ec700c5c65fe04fc8
elisha <namelisha@gmail.com>
2024-12-28 19:02:02 -0800

    Add functions for basic nn module

--- train.py
+++ train.py
@@ -15,5 +15,75 @@ decode = lambda l : ''.join([itos[i] for i in l]) # Given a list of integers, ou
 # Encode entire text dataset and store it into torch.Tensor
 import torch
 
-print(encode('hello world'))
-print(decode(encode('hello world')))
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+
+# Split up data into train and validation sets
+n = int(0.9 * len(data)) # First 90% is training set, remaining 10% is validation set
+train_data = data[:n]
+val_data = data[n:]
+
+# Demoing a single sequence of training with a set of batch and block size
+torch.manual_seed(1337) # For reproducability
+batch_size = 4 # How many independent sequences are processed in parallel
+block_size = 8 # Maximum context length for predictions
+
+def get_batch(split):
+    # generate a small batch of data of inputs x and y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i + block_size] for i in ix])
+    y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
+    return x, y
+
+xb, yb = get_batch('train')
+print('inputs:')
+print(xb.shape)
+print(xb)
+print('targets:')
+print(yb.shape)
+print(yb)
+
+#####
+import torch.nn as nn
+from torch.nn import functional as F
+torch.manual_seed(1337) # For reproducability
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets):
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
+
+        B, T, C = logits.shape
+        logits = logits.view(B * T, C)
+        targets = targets.view(B * T)
+        loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T + 1)
+        return idx
+
+
+    
+m = BigramLanguageModel(vocab_size)
+logits, loss = m(xb, yb)
+print(logits.shape)
+print(loss)
